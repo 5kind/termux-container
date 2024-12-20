c7f84256cd1521c90e9a0d6c9e4b8265aec843a4
5kind <shikabane320@gmail.com>
2024-12-20 21:01:43 +0800

    refactor: Use /dev/socket/termux as termux tmpfs source.

--- service.d/setup-tmpfs.sh
+++ service.d/setup-tmpfs.sh
@@ -1,18 +1,21 @@
-mount_tmpfs() {
-    local dest="$1"
-    local opts=${2:-"mode=1777,nosuid,nodev"}
+TERMUX_SOCKET=/dev/socket/termux
 
-    [ -d "$dest" ] || mkdir -p "$dest"
-    mountpoint -q $(realpath "$dest") || mount tmpfs "$dest" -t tmpfs -o "$opts"
-    chcon --reference=$PREFIX $dest
-    chown $(stat -c "%U:%G" $PREFIX) $dest
+clone_attr() {
+    local src=$1
+    local dest=$2
+    local opts=$3
+    chcon --reference=$src $dest $opts
+    chown $(stat -c "%U:%G" $src) $dest $opts
 }
 
 setup_tmpfs() {
+    # setup tmpfs in /dev/socket/termux
+    mkdir -m 755 -p $TERMUX_SOCKET/run
+    mkdir -m 1777 $TERMUX_SOCKET/tmp $TERMUX_SOCKET/tmp/.X11-unix
+    clone_attr $PREFIX $TERMUX_SOCKET -R
     # make those directories tmpfs
-    mount_tmpfs $PREFIX/tmp
-    mount_tmpfs $PREFIX/tmp/.X11-unix
-    mount_tmpfs $PREFIX/var/run rw,nosuid,nodev,relatime,mode=755
+    mount --bind $TERMUX_SOCKET/tmp $PREFIX/tmp
+    mount --bind $TERMUX_SOCKET/run $PREFIX/var/run
 }
 # main
 setup_tmpfs
