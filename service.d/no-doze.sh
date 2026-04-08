01c3f7a5c464d4c5f0d7b52fc7d62bb9c7b01e13
5kind <shikabane320@gmail.com>
2026-04-09 00:01:22 +0800

    refactor(service.d): remove unused scripts and reorganize to rc.local.d

--- service.d/no-doze.sh
+++
@@ -1,8 +0,0 @@
-NO_DOZE=true        # No auto doze android
-
-no_doze(){
-    dumpsys deviceidle disable
-    echo "PowerManagerService.noSuspend" | tee /sys/power/wake_lock
-}
-
-$NO_DOZE && no_doze
